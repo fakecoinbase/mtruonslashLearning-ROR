b0edcd59cb0c2ec89e4fb5384ad0f23d2e398f48
Michael Truong <mtruon02@mail.uoguelph.ca>
2019-12-13 14:53:19 -0500

    Annotating some files with notes regarding RoR's standards

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  # Hierarchy: index, show, new, edit, create, update, and destroy
 
   def show
     @article = Article.find(params[:id])
@@ -16,6 +17,10 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
+      # :article is a symbol -- being the identifying scope
+      # The fields are parameters which are referencable in
+      # the controller. params[:article] is the data we receive
+      # which would represent our model instance.
       params.require(:article).permit(:title, :text)
     end
 end
