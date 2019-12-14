82a042140af39afaa23c4ddd7686f9a8155e3906
Michael Truong <mtruon02@mail.uoguelph.ca>
2019-12-14 12:37:59 -0500

    Implemented update, edit

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -5,13 +5,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-
   def show
     @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -28,12 +31,24 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
     def article_params
       # :article is a symbol -- being the identifying scope
       # The fields are parameters which are referencable in
       # the controller. params[:article] is the data we receive
       # which would represent our model instance.
+      # We requires that the article exists
+      # and the title and text keys are permitted
       params.require(:article).permit(:title, :text)
     end
 end
