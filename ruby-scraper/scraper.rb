b5f788af5ed66ac027196c17a37a69580ab71fbf
Michael Truong <mtruon02@mail.uoguelph.ca>
2019-12-18 11:36:31 -0500

    Basic interface implementation and polish

--- ruby-scraper/scraper.rb
+++ ruby-scraper/scraper.rb
@@ -1,9 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
 
+module Scraper
+  def scrape
+    raise "Not implemented"
+  end
+end
 
-class Scraper
-  
+class RFDScraper
+  include Scraper
+
   attr_accessor :doc
 
   def initialize(url)
@@ -13,19 +19,25 @@ class Scraper
     puts "Searching: #{url}"
   end
 
-  def search_hot_threads
+  def scrape
     results = []
+
+    # Traverses each thread_info_title HTML
     threads.each do |thread|
+      score = 0
       scoreHTML = thread.css('dl').css('.post_voting').attribute('data-total')
       titleHTML = thread.css('a').css('.topic_title_link').text
-      score = 0
+
       if !scoreHTML.nil?    
         multiplier = (scoreHTML.value[0,1] == '-') ? -1 : 1
         length = scoreHTML.value.length
         score = scoreHTML.value[1,length].to_i * multiplier
       end
+
       results << [titleHTML.strip, score]
     end
+   
+    # Return populated tuple list (thread_title, vote_score)  
     return results
   end
 
@@ -37,7 +49,7 @@ end
 
 # Running the scraper for tests
 url = "http://forums.redflagdeals.com/hot-deals-f9/"
-scraper = Scraper.new(url)
+scraper = RFDScraper.new(url)
 res = scraper.search_hot_threads
 res.each do |thread|
   puts "#{thread[1]}, #{thread[0]}"
