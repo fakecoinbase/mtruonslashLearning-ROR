1c40a2f45d37cb89555096f96e08f75f2375e576
Michael Truong <mtruon02@mail.uoguelph.ca>
2019-12-20 11:24:39 -0500

    Added some checks to stop failing on 503 response

---
+++ ruby-scraper/amazon_scraper.rb
@@ -0,0 +1,55 @@
+require 'nokogiri'
+require 'open-uri'
+
+module Scraper
+  def scrape
+    raise "Not implemented"
+  end
+end
+
+class AmazonScraper
+  include Scraper
+
+  attr_accessor :doc
+  @@user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+
+  def initialize(url)
+    # Static page for exercise
+    begin
+      html = open(url)
+    rescue OpenURI::HTTPError => error
+      response = error.io
+      puts "Received: #{response.status}, failed to proceed with opening the URL"
+      return response
+    end
+    @doc = Nokogiri::HTML(open(html,
+                              "User-Agent" => @user_agent))
+    puts "Searching: #{url}"
+  end
+
+  def scrape
+    results = []
+
+    # Traverses each thread_info_title HTML
+    threads.each do |thread|
+      puts thread
+    end
+   
+    # Return populated tuple list (thread_title, vote_score)  
+    return results
+  end
+
+  private
+    def threads
+      @doc.css('.priceblock_ourprice')
+    end
+end
+
+# Running the scraper for tests
+product_url = "https://www.amazon.ca/gp/product/B00WRDS0AU?pf_rd_p=05326fd5-c43e-4948-99b1-a65b129fdd73&pf_rd_r=ECHQYPX9MEJ7JD9TC7VE"
+scraper = AmazonScraper.new(product_url)
+if scraper.nil?
+  res = scraper.scrape
+else
+  puts "Scraper failed to initialize -- Ending script"
+end
